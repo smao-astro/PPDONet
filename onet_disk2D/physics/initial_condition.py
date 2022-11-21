e16e0f70d190b3882c3d85a8c2eef332d2818e34
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2022-11-21 14:51:06 +0800

    initial_condition.py: fixed bug on FungVThetaIC

--- onet_disk2D/physics/initial_condition.py
+++ onet_disk2D/physics/initial_condition.py
@@ -229,7 +229,9 @@ class FungVThetaIC(IC):
             """
             r = y[..., 0]
             h_over_r = parameters["aspectratio"] * r ** parameters["flaringindex"]
-            ic_values = (1 - 0.5 * h_over_r**2) ** 0.5 * r ** (-0.5)
+            ic_values = (1 - 0.5 * h_over_r**2) ** 0.5 * r ** (
+                -0.5
+            ) - self.omegaframe * r
             return ic_values
 
         return f
