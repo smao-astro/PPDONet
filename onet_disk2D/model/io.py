5512768cb9d07b620f86e457f7b1389045dfbc4b
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2023-03-25 15:54:02 -0700

    update io: respond to JAX API change

--- onet_disk2D/model/io.py
+++ onet_disk2D/model/io.py
@@ -3,7 +3,6 @@ import pickle
 
 import jax
 import numpy as np
-from jax import tree_map
 
 
 def save(
@@ -15,7 +14,7 @@ def save(
     verbose=True,
 ):
     save_dir = pathlib.Path(save_dir)
-    leaves = jax.tree_leaves(var)
+    leaves = jax.tree_util.tree_leaves(var)
     # extract FlatMap structure
     struct = jax.tree_map(lambda t: 0, var)
 
@@ -27,7 +26,7 @@ def save(
         pickle.dump(struct, f)
 
     if verbose:
-        var_shape = tree_map(lambda arr: arr.shape, var)
+        var_shape = jax.tree_map(lambda arr: arr.shape, var)
         print(f"\n{name} with shape :\n{var_shape}")
         print(f"Saved to {save_dir}: {leaves_file}, {struct_file}")
 
@@ -79,14 +78,14 @@ def load(
         struct = pickle.load(f)
         # FlatMap
 
-    leaves, treedef = jax.tree_flatten(struct)
+    leaves, treedef = jax.tree_util.tree_flatten(struct)
     with open(save_dir / leaves_file, "rb") as f:
         leaves = [np.load(f) for _ in leaves]
 
-    var = jax.tree_unflatten(treedef, leaves)
+    var = jax.tree_util.tree_unflatten(treedef, leaves)
 
     if verbose:
-        var_shape = tree_map(lambda arr: arr.shape, var)
+        var_shape = jax.tree_map(lambda arr: arr.shape, var)
         print(f"\n{name} with shape :\n{var_shape}")
         print(f"Loaded from {save_dir}: {leaves_file}, {struct_file}")
 
@@ -99,7 +98,6 @@ def load_params(
     struct_file="params_struct.pkl",
     verbose=True,
 ):
-
     return load(
         name="Params",
         save_dir=save_dir,
@@ -115,7 +113,6 @@ def load_state(
     struct_file="state_struct.pkl",
     verbose=True,
 ):
-
     return load(
         name="State",
         save_dir=save_dir,
