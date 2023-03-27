7a87464f722d4ae95c3ccc9f803d137c37e231d8
Shunyuan Mao <54871380+smao-astro@users.noreply.github.com>
2023-03-27 09:27:26 -0700

    tmp save: improve real_time_prediction_single_var.py

--- visualization/real_time_prediction_single_var.py
+++ visualization/real_time_prediction_single_var.py
@@ -6,11 +6,12 @@ import dash_bootstrap_components as dbc
 import jax.numpy as jnp
 import numpy as np
 import plotly.express as px
-import yaml
+import skimage.transform
 
-import onet_disk2D.model
 import onet_disk2D.grids
+import onet_disk2D.model
 import onet_disk2D.run
+
 
 # todo add guidance
 # todo add model infomation (scrollable window)
@@ -52,11 +53,46 @@ def get_parser():
     return parser
 
 
-def sample_y():
+class CartesianToPolar:
+    def __init__(self, nxy, nr, ntheta, rmin, rmax, theta_min, theta_max):
+        self.nxy = nxy
+        self.nr = nr
+        self.ntheta = ntheta
+        self.rmin = rmin
+        self.rmax = rmax
+        self.theta_min = theta_min
+        self.theta_max = theta_max
+
+    def __call__(self, output_coor):
+        # pixel to coordinate
+        output_coor = 2 * self.rmax * (output_coor / self.nxy - 0.5)
+        """(column, row)"""
+
+        x, y = np.split(output_coor, 2, axis=1)
+        r = np.sqrt(x**2 + y**2)
+        theta = np.arctan2(y, x)
+        """from -pi to pi"""
+
+        # coordinate to pixel
+        r = np.asarray(
+            (r - self.rmin) / (self.rmax - self.rmin) * (self.nr - 1),
+            dtype=int,
+        )
+        theta = np.asarray(
+            (theta - self.theta_min)
+            / (self.theta_max - self.theta_min)
+            * (self.ntheta - 1),
+            dtype=int,
+        )
+
+        return np.hstack([theta, r])
+
+
+def sample_y(num_cell_radial, num_cell_azimuthal):
     ymin = float(job.fargo_setups["ymin"])
     ymax = float(job.fargo_setups["ymax"])
-    ny = int(job.fargo_setups["ny"])
-    nx = int(job.fargo_setups["nx"])
+    ny = num_cell_radial if num_cell_radial else int(job.fargo_setups["ny"])
+    nx = num_cell_azimuthal if num_cell_azimuthal else int(job.fargo_setups["nx"])
     # generate coords
     grids = onet_disk2D.grids.Grids(
         ymin=ymin, ymax=ymax, xmin=-np.pi, xmax=np.pi, ny=ny, nx=nx
@@ -135,7 +171,6 @@ def update_graph(alpha, aspectratio, planetmass):
 
     """
     # todo colorbar ticks should be in LaTeX format
-    # todo improve performance by disable imshow interactivity
     alpha = 10.0**alpha
     planetmass = 10.0**planetmass
     u = jnp.array([alpha, aspectratio, planetmass])[None, :]
@@ -149,60 +184,122 @@ def update_graph(alpha, aspectratio, planetmass):
 
     predict = job.s_pred_fn(job.model.params, job.state, inputs)
     predict = predict.reshape(y_grids.ny, y_grids.nx)
+    nxy = 512
+    cartesian_to_polar = CartesianToPolar(
+        nxy=nxy,
+        nr=len(r),
+        ntheta=len(theta),
+        rmin=min(r),
+        rmax=max(r),
+        theta_min=min(theta),
+        theta_max=max(theta),
+    )
+    predict_in_cartesian = skimage.transform.warp(
+        predict,
+        cartesian_to_polar,
+        output_shape=(nxy, nxy),
+        order=3,
+    )
 
-    vmin = float(np.min(predict))
-    vmax = float(np.max(predict))
+    # vmin = float(np.nanmin(predict_in_cartesian))
+    # vmax = float(np.nanmax(predict_in_cartesian))
+    # a = 1.0 / (vmax - vmin)
+    # b = -a * vmin
+    # vmid = b
+    vmin = -5
+    vmax = 0.2
     a = 1.0 / (vmax - vmin)
     b = -a * vmin
     vmid = b
 
     # px.imshow
+    x = y = np.linspace(-max(r), max(r), nxy)
     fig = px.imshow(
-        predict,
-        x=theta,
-        y=r,
+        predict_in_cartesian,
+        x=x,
+        y=y,
         range_color=[vmin, vmax],
         # aspect=1/3,
         origin="lower",
+        aspect="equal",
     )
+    r_lim = 1.2
+    fig.update_xaxes(range=[-r_lim, r_lim])
+    fig.update_yaxes(range=[-r_lim, r_lim], autorange=False)
+    fig.update_layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
     fig.update_layout(
         margin=dict(t=0, b=0),
     )
+    # try to improve performance by disable imshow interactivity
     fig.update_traces(hovertemplate=None, hoverinfo="skip")
     fig.update_xaxes(
         {
-            "title": {"text": "Azimuth", "standoff": 25, "font": {"size": 35}},
-            "scaleanchor": "y",
-            "scaleratio": y_grids.ny / y_grids.nx,
+            "title": {"text": "X", "standoff": 25, "font": {"size": 35}},
+            # "scaleanchor": "y",
+            # "scaleratio": y_grids.ny / y_grids.nx,
         }
     )
     fig.update_yaxes(
         {
-            "title": {"text": "Radius", "standoff": 25},
+            "title": {"text": "Y", "standoff": 25},
         }
     )
-    xaxes = {
-        "tickmode": "array",
-        "tickvals": [-np.pi / 2, 0.0, np.pi / 2],
-        "ticktext": [
-            r"$\huge{-\frac{1}{2}\pi}$",
-            r"$\huge{0}$",
-            r"$\huge{\frac{1}{2}\pi}$",
-        ],
-    }
-    fig.update_xaxes(xaxes)
-    colorbar_ticks = np.arange(np.ceil(vmin), np.ceil(vmax), 1)
+    # xaxes = {
+    #     "tickmode": "array",
+    #     "tickvals": [-np.pi / 2, 0.0, np.pi / 2],
+    #     "ticktext": [
+    #         r"$\huge{-\frac{1}{2}\pi}$",
+    #         r"$\huge{0}$",
+    #         r"$\huge{\frac{1}{2}\pi}$",
+    #     ],
+    # }
+    # fig.update_xaxes(xaxes)
+    # if np.ceil(vmax) - np.ceil(vmin) < 3:
+    #     colorbar_ticks = np.linspace(np.round(vmin, 2), np.round(vmax, 2), 4, endpoint=False)
+    #     fig.update_coloraxes(
+    #         {
+    #             "colorscale": [(0.0, "blue"), (vmid, "white"), (1.0, "red")],
+    #             "colorbar": {
+    #                 "tickvals": colorbar_ticks,
+    #                 "ticktext": [f"{10 ** v:.1f}" for v in colorbar_ticks],
+    #                 "len": 0.8,
+    #             },
+    #         }
+    #     )
+    # else:
+    #     colorbar_ticks = np.arange(np.ceil(vmin), np.ceil(vmax), 1)
+    #     # layout
+    #     fig.update_coloraxes(
+    #         {
+    #             "colorscale": [(0.0, "blue"), (vmid, "white"), (1.0, "red")],
+    #             "colorbar": {
+    #                 "tickvals": colorbar_ticks,
+    #                 "ticktext": [f"{10**v:.0e}" for v in colorbar_ticks],
+    #                 "len": 0.8,
+    #             },
+    #         }
+    #     )
+    colorbar_ticks = [-5, -4, -3, -2, -1, vmax]
     # layout
     fig.update_coloraxes(
         {
             "colorscale": [(0.0, "blue"), (vmid, "white"), (1.0, "red")],
             "colorbar": {
                 "tickvals": colorbar_ticks,
-                "ticktext": [f"{10**v:.0e}" for v in colorbar_ticks],
+                "ticktext": [
+                    # f"{10**v:.0e}" if v < -2 else f"{10**v:.1f}" for v in colorbar_ticks
+                    "10e-5",
+                    "10e-4",
+                    "0.001",
+                    "0.01",
+                    "0.1",
+                    f"{10**vmax:.1f}",
+                ],
                 "len": 0.8,
             },
         }
     )
+
     fig.update_layout({"font": {"size": 30, "family": "Times New Roman"}})
 
     return fig
@@ -269,7 +366,7 @@ if __name__ == "__main__":
     job = onet_disk2D.run.JOB(job_args)
     job.load_model(model_dir)
 
-    y_grids = sample_y()
+    y_grids = sample_y(predict_args.num_cell_radial, predict_args.num_cell_azimuthal)
 
     app = dash.Dash(
         __name__,
@@ -343,7 +440,8 @@ if __name__ == "__main__":
                 width=3,
                 style={"font-size": "30px"},
             ),  # bar
-            dbc.Col(dbc.Spinner(graph), width=True, align="center"),  # graph
+            # dbc.Col(dbc.Spinner(graph), width=True, align="center"),  # graph
+            dbc.Col(graph, width=True, align="center"),  # graph
         ],
         justify="center",
         # style={"height": "60%"},
